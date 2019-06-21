c9679f475fcd45c33e09b842438dddc19be2fb61
nareshkumar288898 <nk288898@gmail.com>
2019-06-21 16:37:39 +0530

    Manager Class Created

---
+++ Manager.cs
@@ -0,0 +1,17 @@
+using System;
+Using namespace Project;
+public class Manager
+{
+public int ManagerId
+{
+set; get;
+}
+ public string Managername
+ {
+ set;get;
+ }
+ public string Designation
+ {
+ set;get;
+ }
+ }
