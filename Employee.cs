176d216b2cd567010c527d91c577bc0a627e0be2
nareshkumar2888 <38119904+nareshkumar2888@users.noreply.github.com>
2019-06-21 14:09:24 +0530

    Created Employee Class

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+using System;
+Using namespace Project;
+public class Employee
+{
+public int EmployeeId
+{
+set; get;
+}
+ public string Employeename
+ {
+ set;get;
+ }
+ public string Designation
+ {
+ set;get;
+ }
+ }
