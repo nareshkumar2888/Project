8ce4244009504759ad6109b4e8b6b2a7342a04f2
nareshumar288898 <nk288898@gmail.com>
2019-06-24 10:04:05 +0530

    Brij,Dev

--- Master.cs
+++ Master.cs
@@ -1,31 +1,34 @@
 using System;
-Using namespace Project;
+using namespace Project;
 public class Master
 {
 
- public string Mastername
- {
- set;get;
- }
-<<<<<<< HEAD
- public String Designation{
-=======
-  public String Designation{
->>>>>>> Decorate_Name
-	 set;get;
- }
- public void Disply(){
-	 Console.WriteLine("Enter Master Name:");
-	 Mastername=Console.ReadLine();
- Console.WriteLine("Mr"+Mastername);
-<<<<<<< HEAD
- Console.WriteLine("Enter Your Designation:");
-=======
-  Console.WriteLine("Enter Your Designation:");
->>>>>>> Decorate_Name
-Designation=Console.ReadLine();
-Console.WriteLine(Mastername,Designation);
-//
- }
+    public string Mastername
+    {
+        set; get;
+    }
+    //<<<<<<< HEAD
+
+    //=======
+    public String Designation
+    {
+        //>>>>>>> Decorate_Name
+        set; get;
+    }
+    public void Disply()
+    {
+
+        Console.WriteLine("Enter Master Name:");
+        Mastername = Console.ReadLine();
+        Console.WriteLine("Mr" + Mastername);
+        //<<<<<<< HEAD
+        Console.WriteLine("Enter Your Designation:");
+        //=======
+        Console.WriteLine("Enter Your Designation:");
+        //>>>>>>> Decorate_Name
+        Designation = Console.ReadLine();
+        Console.WriteLine(Mastername, Designation);
+
+    }
+}
  
- }
